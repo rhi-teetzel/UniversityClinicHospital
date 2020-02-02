cf68dd48d00656d7ca745cb8801fab941f70b5b1
Tatyana Bailey <btatyana45@gmail.com>
2020-02-02 13:42:23 -0500

    started making a doctor.CareForPatientsTest and found issue in current method

---
+++ UniversityClinicHospital.Tests/DoctorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UniversityClinicHospital.Tests
+{
+    public class DoctorTests
+    {
+        [Fact]
+        public void CareForPatients_Health_Increases()
+        {
+            //Arrange
+            var patient = new Patient("");
+            var doctor = new Doctor("", 2, "");
+            var testHealth = patient.HealthLevel;
+
+            //Act
+            doctor.CareForPatients();
+
+            //Assert
+            Assert.Equal(testHealth + 20, patient.HealthLevel);
+        }
+
+    }
+}
